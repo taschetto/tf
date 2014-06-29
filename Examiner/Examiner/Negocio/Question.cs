cd6f1ae7f1b32265d7f3984bac8db076c37d9be8
gabrielvargas92 <gabriel.macielv@hotmail.com>
2014-06-29 14:00:30 -0300

    Classes base / parcial

---
+++ Examiner/Examiner/Negocio/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examiner.Negocio
+{
+    class Question
+    {
+        private String _enunciado;
+        private List<String> _alternativas;
+        private String _resp;
+        private String _feedback;
+
+        public String Enunciado { set; get; }
+        public List<String> Alternativa { set; get; }
+        public String Resposta { get; set; }
+        public String Feedback { get; set; }
+
+        public Question(String enunciado, List<String> alt , String resp, String feed)
+        {
+            _enunciado = enunciado;
+            _alternativas = alt;
+            _resp = resp;
+            _feedback = feed;
+        }
+
+
+    }
+}
