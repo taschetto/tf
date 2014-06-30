4ea80155361140bdd33beb9e9eebd3adcf40b634
Guilherme Taschetto <gtaschetto@gmail.com>
2014-06-29 23:15:35 -0300

    Organização

--- Examiner/Examiner/Persistence/CategoryDB.cs
+++ Examiner/Examiner/Persistence/CategoryDB.cs
@@ -1,57 +1,32 @@
-﻿using System.Collections.Generic;
-
-namespace Examiner.Persistence
+﻿namespace Examiner.Persistence
 {
   using Examiner.Business;
+  using Examiner.Business.Models;
+  using System.Collections.Generic;
 
   public class CategoryDB : CategoryDAO
   {
-    public void Add(Business.Models.Category t)
+    public void Add(Category t)
     {
       throw new System.NotImplementedException();
     }
 
-    public void Update(Business.Models.Category t)
+    public void Update(Category t)
     {
       throw new System.NotImplementedException();
     }
 
-    public void Delete(Business.Models.Category t)
+    public void Delete(Category t)
     {
       throw new System.NotImplementedException();
     }
 
-      public void Add(Category t)
-      {
-          throw new System.NotImplementedException();
-      }
-
-      public void Update(Category t)
-      {
-          throw new System.NotImplementedException();
-      }
-
-      public void Delete(Category t)
-      {
-          throw new System.NotImplementedException();
-      }
-
-      List<Category> BaseDAO<Category>.GetAll()
-      {
-          throw new System.NotImplementedException();
-      }
-
-      Category BaseDAO<Category>.GetById(int id)
-      {
-          throw new System.NotImplementedException();
-      }
-
-      public System.Collections.Generic.List<Business.Models.Category> GetAll()
+    public List<Category> GetAll()
     {
       throw new System.NotImplementedException();
     }
 
-    public Business.Models.Category GetById(int id)
+    public Category GetById(int id)
     {
       throw new System.NotImplementedException();
     }
