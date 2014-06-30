82b3c0f2d5d3c0fd6aba66fe8357c05ac0863dcb
gabrielvargas92 <gabriel.macielv@hotmail.com>
2014-06-29 22:37:28 -0300

    Organização de nomes

--- Examiner/Examiner/Business/DAOs/CategoryDAO.cs
+++ Examiner/Examiner/Business/DAOs/CategoryDAO.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Examiner.Business.Models
+namespace Examiner.Business
 {
-    class CreateExamFactory
-    {
-    }
+  interface CategoryDAO
+  {
+  }
 }
