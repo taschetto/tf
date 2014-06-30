795b08645f39e0a59cc13c647d137f6736e4e109
gabrielvargas92 <gabriel.macielv@hotmail.com>
2014-06-30 04:40:59 -0300

    DAO Exception

---
+++ Examiner/Examiner/Business/Models/DAOExcepton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examiner.Business.Models
+{
+    class DAOExcepton : Exception
+    {
+        public DAOExcepton(string message) : base(message)
+        {
+        }
+    }
+
+    
+}
