5dfed067f6c4651faf0286f3dc16a43fb1e07c3b
gabrielvargas92 <gabriel.macielv@hotmail.com>
2014-06-30 16:44:27 -0300

    Padronização de nomes

---
+++ Examiner/Examiner/Business/DAOs/IBaseDAO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Examiner.Business.DAOs
+{
+    public interface IBaseDao<T>
+  {
+    void Add(T t);
+
+    void Update(T t);
+
+    void Delete(T t);
+
+    List<T> GetAll();
+
+    T GetById(int id);
+  }
+}
