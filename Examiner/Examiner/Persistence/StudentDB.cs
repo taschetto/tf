0099dcf0ba4efc641de9acc9fc051c71f9cb048e
gabrielvargas92 <gabriel.macielv@hotmail.com>
2014-06-30 16:37:32 -0300

    Usings redundantes

--- Examiner/Examiner/Persistence/StudentDB.cs
+++ Examiner/Examiner/Persistence/StudentDB.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Windows.Media.Imaging;
 using Examiner.Business.DAOs;
 using Examiner.Business.Models;
 
