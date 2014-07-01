e59a8de50fa8a07408380a658c5b878c9bb12065
gabrielvargas92 <gabriel.macielv@hotmail.com>
2014-06-30 21:46:32 -0300

    Question

--- Examiner/Examiner/Persistence/QuestionDB.cs
+++ Examiner/Examiner/Persistence/QuestionDB.cs
@@ -1,165 +1,171 @@
-﻿
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using Examiner.Business.DAOs;
-using Examiner.Business.Models;
-
-
-namespace Examiner.Persistence
+﻿namespace Examiner.Persistence
 {
+  using System.Collections.Generic;
+  using System.Data;
+  using System.Data.SqlClient;
+  using Examiner.Business.DAOs;
+  using Examiner.Business.Exceptions;
+  using Examiner.Business.Models;
 
-    public class QuestionDB : IQuestionDao
+  public class QuestionDB : IQuestionDao
+  {
+
+    private static QuestionDB instance = null;
+
+    public static QuestionDB Instance
+    {
+      get
+      {
+        if (instance == null)
+        {
+          instance = new QuestionDB();
+        }
+
+        return instance;
+      }
+    }
+
+
+    public void Add(Question t)
+    {
+      try
+      {
+        SqlCommand comando = new SqlCommand();
+        comando.CommandType = CommandType.Text;
+        comando.CommandText =
+            string.Format("INSERT [Question] (questionContent, feedbackContent, rightAlternative) " +
+                          "VALUES('{0}','{1}','{2}')",
+                            t.QuestionContent,
+                            t.FeedbackContent,
+                            t.RightAlternative
+                            );
+
+        comando.Connection = ConnectionDB.CreateConnection();
+
+        comando.Connection.Open();
+        comando.ExecuteNonQuery();
+        comando.Connection.Close();
+      }
+      catch (SqlException e)
+      {
+        throw new DAOException(e);
+      }
+    }
+
+    public void Update(Question t)
     {
 
-        private static QuestionDB _referenceQuestionDb = null;
+      try
+      {
+        SqlCommand comando = new SqlCommand();
+        comando.CommandType = CommandType.Text;
+        comando.CommandText =
+           string.Format("UPDATE [Question] SET questionContent = {0}, feedbackContent = {1}, rightAlternative= {2}) " +
+                         "WHERE = {3}",
+                           t.QuestionContent,
+                           t.FeedbackContent,
+                           t.RightAlternative,
+                           t.Id
+                           );
+        comando.Connection = ConnectionDB.CreateConnection();
 
-        public static QuestionDB GetInstance()
+        comando.Connection.Open();
+        comando.ExecuteNonQuery();
+        comando.Connection.Close();
+
+      }
+      catch (SqlException e)
+      {
+        throw new DAOException(e);
+      }
+
+    }
+
+    public void Delete(Question t)
+    {
+      try
+      {
+        SqlCommand comando = new SqlCommand();
+        comando.CommandType = CommandType.Text;
+        comando.CommandText =
+           string.Format("DELETE FROM [Question] WHERE id_question = {0}",
+                           t.Id
+                           );
+        comando.Connection = ConnectionDB.CreateConnection();
+
+        comando.Connection.Open();
+        comando.ExecuteNonQuery();
+        comando.Connection.Close();
+
+      }
+      catch (SqlException e)
+      {
+        throw new DAOException(e);
+      }
+    }
+
+    public List<Question> GetAll()
+    {
+
+        try
         {
-            if (_referenceQuestionDb == null)
+            SqlCommand comando = new SqlCommand();
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = "select * from Question";
+
+            comando.Connection = ConnectionDB.CreateConnection();
+            SqlDataAdapter adapter = new SqlDataAdapter(comando);
+            DataTable table = new DataTable();
+
+            List<Question> questions = new List<Question>();
+
+            if (table.Rows.Count == 0)
             {
-                _referenceQuestionDb = new QuestionDB();
+                return null;
             }
-            return (_referenceQuestionDb);
+
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                Question question= new Question(
+                    (int)table.Rows[i][0],
+                    (string)table.Rows[i][1],
+                    (string)table.Rows[i][2],
+                    AlternativeDB.Instance.GetById((int)table.Rows[i][3])
+                    );
+                questions.Add(question);
+            }
+
+            return questions;
         }
-
-
-        public void Add(Question t)
+        catch (SqlException e)
         {
-            try
-            {
-                SqlCommand comando = new SqlCommand();
-                comando.CommandType = CommandType.Text;
-                comando.CommandText =
-                    string.Format("INSERT [Question] (questionContent, feedbackContent, rightAlternative) " +
-                                  "VALUES('{0}','{1}','{2}')",
-                                    t.QuestionContent,
-                                    t.FeedbackContent,
-                                    t.RightAlternative
-                                    );
-
-                comando.Connection = ConnectionDB.CreateConnection();
-
-                comando.Connection.Open();
-                comando.ExecuteNonQuery();
-                comando.Connection.Close();
-            }
-            catch (SqlException e)
-            {
-                throw new DAOException(e.Message);
-            }
-        }
-
-        public void Update(Question t)
-        {
-
-            try
-            {
-                SqlCommand comando = new SqlCommand();
-                comando.CommandType = CommandType.Text;
-                comando.CommandText =
-                   string.Format("UPDATE [Question] SET questionContent = {0}, feedbackContent = {1}, rightAlternative= {2}) " +
-                                 "WHERE = {3}",
-                                   t.QuestionContent,
-                                   t.FeedbackContent,
-                                   t.RightAlternative,
-                                   t.Id
-                                   );
-                comando.Connection = ConnectionDB.CreateConnection();
-
-                comando.Connection.Open();
-                comando.ExecuteNonQuery();
-                comando.Connection.Close();
-
-            }
-            catch (SqlException e)
-            {
-                throw new DAOException(e.Message);
-            }
-
-        }
-
-        public void Delete(Question t)
-        {
-            try
-            {
-                SqlCommand comando = new SqlCommand();
-                comando.CommandType = CommandType.Text;
-                comando.CommandText =
-                   string.Format("DELETE FROM [Question] WHERE id_question = {0}",
-                                   t.Id
-                                   );
-                comando.Connection = ConnectionDB.CreateConnection();
-
-                comando.Connection.Open();
-                comando.ExecuteNonQuery();
-                comando.Connection.Close();
-
-            }
-            catch (SqlException e)
-            {
-                throw new DAOException(e.Message);
-            }
-        }
-
-        public List<Question> GetAll()
-        {
-
-            try
-            {
-                SqlCommand comando = new SqlCommand();
-                comando.CommandType = CommandType.Text;
-                comando.CommandText = string.Format("select * from [Question]");
-
-
-                comando.Connection = ConnectionDB.CreateConnection();
-                SqlDataAdapter adapter = new SqlDataAdapter(comando);
-                DataTable table = new DataTable();
-
-                adapter.Fill(table);
-
-
-                /*Falta implementar o restante
-                 * 
-                 * 
-                 * 
-                 * 
-                 * 
-                 * 
-                 */
-
-                comando.Connection.Open();
-                comando.ExecuteNonQuery();
-                comando.Connection.Close();
-
-            }
-            catch (SqlException e)
-            {
-                throw new DAOException(e.Message);
-            }
-        }
-
-        public Question GetById(int id)
-        {
-            try
-            {
-                SqlCommand comando = new SqlCommand();
-                comando.CommandType = CommandType.Text;
-                comando.CommandText =
-                   string.Format("SELECT * FROM  Question WHERE id_question = {0}",
-                                  id
-                                   );
-                comando.Connection = ConnectionDB.CreateConnection();
-
-                comando.Connection.Open();
-                comando.ExecuteNonQuery();
-                comando.Connection.Close();
-
-            }
-            catch (SqlException e)
-            {
-                throw new DAOException(e.Message);
-            }
+            throw new DAOException(e);
         }
     }
+
+    public Question GetById(int id)
+    {
+      try
+      {
+        SqlCommand comando = new SqlCommand();
+        comando.CommandType = CommandType.Text;
+        comando.CommandText =
+           string.Format("SELECT * FROM  Question WHERE id_question = {0}",
+                          id
+                           );
+        comando.Connection = ConnectionDB.CreateConnection();
+
+        comando.Connection.Open();
+        comando.ExecuteNonQuery();
+        comando.Connection.Close();
+
+      }
+      catch (SqlException e)
+      {
+        throw new DAOException(e);
+      }
+
+      return null;
+    }
+  }
 }
