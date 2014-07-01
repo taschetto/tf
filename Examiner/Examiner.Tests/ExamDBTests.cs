4b1a0e8b0072a9815f70a585c7b14ec1a3c4668b
Guilherme Taschetto <gtaschetto@gmail.com>
2014-07-01 16:57:42 -0300

    Adicionado teste unitário para ExamDB

---
+++ Examiner/Examiner.Tests/ExamDBTests.cs
@@ -0,0 +1,61 @@
+﻿namespace Examiner.Tests
+{
+  using System;
+  using System.Linq;
+  using Examiner.Business.Models;
+  using Examiner.Persistence;
+  using NUnit.Framework;
+
+  [TestFixture]
+  public class ExamDBTests
+  {
+    [Test]
+    public void AddExam()
+    {
+      var exam = new Exam(0, 5, true, string.Empty);
+      Assert.AreEqual(true, ExamDB.Instance.Add(exam));
+    }
+
+    [Test]
+    public void GetAllExam()
+    {
+      var exams = ExamDB.Instance.GetAll();
+      Assert.GreaterOrEqual(exams.Count, 0);
+    }
+
+    [Test]
+    public void DeleteExam()
+    {
+      var exams = ExamDB.Instance.GetAll();
+      if (exams.Count > 0)
+      {
+        Exam exam = exams.First();
+        Assert.AreEqual(true, ExamDB.Instance.Delete(exam));
+      }
+    }
+
+    [Test]
+    public void UpdateExam()
+    {
+      var exams = ExamDB.Instance.GetAll();
+      if (exams.Count > 0)
+      {
+        Exam exam = exams.First();
+        exam.QuestionCount = 15;
+        Assert.AreEqual(true, ExamDB.Instance.Update(exam));
+      }
+    }
+
+    [Test]
+    public void GetById()
+    {
+      var exams = ExamDB.Instance.GetAll();
+      if (exams.Count > 0)
+      {
+        Exam exam1 = exams.First();
+        Exam exam2 = ExamDB.Instance.GetById(exam1.Id);
+        Assert.AreEqual(true, exam1.Equals(exam2));
+      }
+    }
+  }
+}
